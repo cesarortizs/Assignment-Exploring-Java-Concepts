658df20997406af2a7e6c4e0ecf8a24d6dae0f86
cesarortizs <cgos2112@gmail.com>
2025-04-22 21:37:14 -0600

    Part 1 completed

---
+++ Part-1-Exploring-Variables-and-Primitive-Types/Part1/src/main/java/Part1/Main.java
@@ -0,0 +1,15 @@
+package Part1;
+
+public class Main {
+    public static void main(String[] args) {
+        int age = 26;
+        double height = 67;
+        char initial = 'C';
+        boolean isStudent = true;
+
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(initial);
+        System.out.println(isStudent);
+    }
+}
